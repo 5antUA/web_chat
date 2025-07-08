15081dda5e17063349125be09107c8a13c204f4f
5antUA <rostykkashper2@gmail.com>
2025-07-08 12:34:23 +0300

    gitignore changes

--- back/src/main.rs
+++ back/src/main.rs
@@ -3,7 +3,7 @@ use std::process;
 #[actix_web::main]
 async fn main() {
     if let Err(error) = back::run_server().await {
-        println!("{}", error);
+        eprintln!("{}", error);
         process::exit(1);
     }
 }
